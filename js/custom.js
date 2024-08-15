66f165beef898eb4dded90027ee3851967a194e0
cimrikabaj <bajcimi@gmail.com>
2024-08-15 07:20:47 +0545

    board and menu update

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
         // AOS script
         AOS.init();
+
+        $('.mob-menu .icon-bars').on('click', function () {
+                $('.nav-bar').toggleClass('open');
+                $('.mob-menu').toggleClass('open');
+            })
 })
 
